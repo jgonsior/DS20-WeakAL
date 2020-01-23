55b35c699d9dd554b38bde63df94ebfc946c5aa1
Julius Gonsior <julius@gaenseri.ch>
2020-01-23 18:11:35 +0100

    Fixed uncertainty sampling

--- sampling_strategies/uncertaintySampling.py
+++ sampling_strategies/uncertaintySampling.py
@@ -1,7 +1,10 @@
-import numpy as np
+from itertools import chain
 from pprint import pprint
-from activeLearner import ActiveLearner
+
+import numpy as np
 from scipy.stats import entropy
+
+from activeLearner import ActiveLearner
 
 
 class UncertaintySampler(ActiveLearner):
@@ -11,11 +14,16 @@ class UncertaintySampler(ActiveLearner):
     def setClassifierClasses(self, classes):
         self.classifier_classes = classes
 
-    def calculate_next_query_indices(self, X_train_unlabeled, X_train_labeled,
-                                     Y_train_labeled, *args):
+    def calculate_next_query_indices(self, X_train_unlabeled_cluster_indices,
+                                     *args):
+        # merge indices from all clusters together and take the n most uncertain ones from them
+        X_train_unlabeled_indices = list(
+            chain(*list(X_train_unlabeled_cluster_indices.values())))
+
         # recieve predictions and probabilitys
         # for all possible classifications of classifier
-        Y_temp_proba = self.clf_list[0].predict_proba(X_train_unlabeled)
+        Y_temp_proba = self.clf_list[0].predict_proba(
+            self.data_storage.X_train_unlabeled.loc[X_train_unlabeled_indices])
 
         if self.strategy == 'least_confident':
             result = 1 - np.amax(Y_temp_proba, axis=1)
@@ -25,9 +33,9 @@ class UncertaintySampler(ActiveLearner):
         elif self.strategy == 'entropy':
             result = np.apply_along_axis(entropy, 1, Y_temp_proba)
 
-        #  pprint(result)
-        #  print(np.argsort(-result))
+        # sort X_train_unlabeled_indices by argsort
+        argsort = np.argsort(-result)
+        query_indices = np.array(X_train_unlabeled_indices)[argsort]
 
-        query_indices = np.argsort(-result)[:self.nr_queries_per_iteration]
         # return smallest probabilities
-        return query_indices
+        return query_indices[:self.data_storage.nr_queries_per_iteration]
