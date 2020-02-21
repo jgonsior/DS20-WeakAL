101490f442de3ff3933f9645be88c34c975447d3
Julius Gonsior <julius@gaenseri.ch>
2020-02-21 18:16:39 +0100

    More stats

--- al_hyper_stats.py
+++ al_hyper_stats.py
@@ -14,7 +14,7 @@ import numpy as np
 import pandas as pd
 import peewee
 from evolutionary_search import EvolutionaryAlgorithmSearchCV
-from json_tricks import dumps
+from json_tricks import dumps, loads
 from scipy.stats import randint, uniform
 from sklearn.base import BaseEstimator
 from sklearn.datasets import load_iris
@@ -27,11 +27,11 @@ from cluster_strategies import (DummyClusterStrategy,
                                 RandomClusterStrategy,
                                 RoundRobinClusterStrategy)
 from dataStorage import DataStorage
-from experiment_setup_lib import (Logger,
+from experiment_setup_lib import (ExperimentResult, Logger,
                                   classification_report_and_confusion_matrix,
+                                  get_db, init_logging,
                                   load_and_prepare_X_and_Y, standard_config,
-                                  store_pickle, store_result, init_logging,
-                                  get_db, ExperimentResult)
+                                  store_pickle, store_result)
 from sampling_strategies import (BoundaryPairSampler, CommitteeSampler,
                                  RandomSampler, UncertaintySampler)
 
@@ -44,5 +44,21 @@ best_result = ExperimentResult.select().order_by(
     ExperimentResult.fit_score.desc()).limit(10)
 
 for result in best_result:
-    print("{:6,d} {:6.2%} {:6.2%}".format(result.amount_of_user_asked_queries,
-                                          result.acc_test, result.fit_score))
+    metrics = loads(result.metrics_per_al_cycle)
+
+    amount_of_clusters = 0
+    amount_of_certainties = 0
+    for recommendation, query_length in zip(metrics['recommendation'],
+                                            metrics['query_length']):
+        if recommendation == 'U':
+            amount_of_certainties += 1
+        if recommendation == 'C':
+            amount_of_clusters += 1
+
+    print("{:>25} {:>25} {:4,d} {:4,d} {:2,d} {:5,d} {:6.2%} {:6.2%}".format(
+        result.sampling, result.cluster, amount_of_clusters,
+        amount_of_certainties, result.allow_recommendations_after_stop,
+        result.amount_of_user_asked_queries, result.acc_test,
+        result.fit_score))
+
+# print for best run all the metrics from active_learner.py
