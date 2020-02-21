6ba96a62718bc275649df793553946b60055f4b5
Julius Gonsior <julius@gaenseri.ch>
2020-02-21 14:08:40 +0100

    Started stats

---
+++ al_hyper_stats.py
@@ -0,0 +1,47 @@
+import argparse
+import contextlib
+import datetime
+import io
+import logging
+import multiprocessing
+import os
+import random
+import sys
+from itertools import chain, combinations
+from timeit import default_timer as timer
+
+import numpy as np
+import pandas as pd
+import peewee
+from evolutionary_search import EvolutionaryAlgorithmSearchCV
+from json_tricks import dumps
+from scipy.stats import randint, uniform
+from sklearn.base import BaseEstimator
+from sklearn.datasets import load_iris
+from sklearn.model_selection import (GridSearchCV, ParameterGrid,
+                                     RandomizedSearchCV, train_test_split)
+from sklearn.preprocessing import LabelEncoder
+
+from cluster_strategies import (DummyClusterStrategy,
+                                MostUncertainClusterStrategy,
+                                RandomClusterStrategy,
+                                RoundRobinClusterStrategy)
+from dataStorage import DataStorage
+from experiment_setup_lib import (Logger,
+                                  classification_report_and_confusion_matrix,
+                                  load_and_prepare_X_and_Y, standard_config,
+                                  store_pickle, store_result, init_logging,
+                                  get_db, ExperimentResult)
+from sampling_strategies import (BoundaryPairSampler, CommitteeSampler,
+                                 RandomSampler, UncertaintySampler)
+
+standard_config = standard_config([])
+
+init_logging(standard_config.output_dir)
+db = get_db()
+
+best_result = ExperimentResult.select().order_by(
+    ExperimentResult.fit_score.desc()).limit(10)
+
+for result in best_result:
+    print(result.fit_score)
--- experiment_setup_lib.py
+++ experiment_setup_lib.py
@@ -6,23 +6,97 @@ import os
 import pickle
 import random
 import sys
-
+import logging
 import numpy as np
 import pandas as pd
 import sklearn.metrics
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler, RobustScaler
+import datetime
+import peewee
+
+db = peewee.SqliteDatabase('experiment_results.db')
 
 
-def divide_data(self, test_fraction, start_set_size):
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
 
+
+class ExperimentResult(BaseModel):
+    id_field = peewee.AutoField()
+
+    # hyper params
+    dataset_path = peewee.TextField()
+    classifier = peewee.TextField()
+    cores = peewee.IntegerField()
+    output_dir = peewee.TextField()
+    test_fraction = peewee.FloatField()
+    sampling = peewee.TextField()
+    random_seed = peewee.IntegerField()
+    cluster = peewee.TextField()
+    nr_learning_iterations = peewee.IntegerField()
+    nr_queries_per_iteration = peewee.IntegerField()
+    start_set_size = peewee.FloatField()
+    with_uncertainty_recommendation = peewee.BooleanField()
+    with_cluster_recommendation = peewee.BooleanField()
+    with_snuba_lite = peewee.BooleanField()
+    uncertainty_recommendation_certainty_threshold = peewee.FloatField(
+        null=True)
+    uncertainty_recommendation_ratio = peewee.FloatField(null=True)
+    snuba_lite_minimum_heuristic_accuracy = peewee.FloatField(null=True)
+    cluster_recommendation_minimum_cluster_unity_size = peewee.FloatField(
+        null=True)
+    cluster_recommendation_ratio_labeled_unlabeled = peewee.FloatField(
+        null=True)
+    metrics_per_al_cycle = peewee.TextField()  # json string
+    amount_of_user_asked_queries = peewee.IntegerField()
+    allow_recommendations_after_stop = peewee.BooleanField()
+    stopping_criteria_uncertainty = peewee.FloatField()
+    stopping_criteria_acc = peewee.FloatField()
+    stopping_criteria_std = peewee.FloatField()
+
+    # information of hyperparam run
+    experiment_run_date = peewee.DateTimeField(default=datetime.datetime.now)
+    fit_time = peewee.TextField()  # timedelta
+    confusion_matrix_test = peewee.TextField()  # json
+    confusion_matrix_train = peewee.TextField()  # json
+    classification_report_train = peewee.TextField()  # json
+    classification_report_test = peewee.TextField()  # json
+    acc_train = peewee.FloatField()
+    acc_test = peewee.FloatField()
+    fit_score = peewee.FloatField()
+
+
+def get_db():
+    # create databases for storing the results
+
+    db.connect()
+    db.create_tables([ExperimentResult])
+
+    return db
+
+
+def init_logging(output_dir):
+    logging_file_name = output_dir + "/" + str(
+        datetime.datetime.now()) + "al_hyper_search.txt"
+
+    logging.basicConfig(
+        filename=logging_file_name,
+        filemode='a',
+        level=logging.INFO,
+        format="[%(process)d] [%(asctime)s] %(levelname)s: %(message)s")
+
+
+def divide_data(test_fraction, start_set_size):
     # split data
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=test_fraction)
 
-    len_test = len(self.X_test)
-    print("size of test set: %i = %1.2f" % (len_test, len_test / len_test))
+    len_test = len(X_test)
+    logging.info("size of test set: %i = %1.2f" %
+                 (len_test, len_test / len_test))
     return X_train, X_test, Y_train, Y_test
 
 
@@ -179,8 +253,8 @@ def classification_report_and_confusion_matrix(clf,
             labels=[i for i in range(len(label_encoder.classes_))],
             target_names=label_encoder.classes_)
 
-        print(clf_report_string)
-        print(conf_matrix)
+        logging.info(clf_report_string)
+        logging.info(conf_matrix)
     else:
         return clf_report, conf_matrix
 
