6af4a217f7801d7c5f7afe5cfda7195da5164f53
Julius Gonsior <julius@gaenseri.ch>
2020-04-03 14:33:07 +0200

    Oho

--- plot/plotLatex.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python plotter.py --strategy sheet_uncertainty_entropy --output results/shuffle_5 --wishedPlots sampling_strategies&
-python plotter.py --strategy sheet_uncertainty_entropy --output results/shuffle_5 --wishedPlots batch_sizes&
-python plotter.py --strategy sheet_uncertainty_entropy --output results/shuffle_5 --wishedPlots start_set_sizes&
-python plotter.py --strategy committee --output results/shuffle_5 --wishedPlots stopping --stop_uncertainty 0.6 --stop_std 0.015 --stop_acc 0.95&
-python plotter.py --strategy sheet_committee --output results/shuffle_5 --wishedPlots stopping --stop_uncertainty 0.5 --stop_std 0.049 --stop_acc 0.95
