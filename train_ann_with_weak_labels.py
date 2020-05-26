e122ddbde2bd2d1924e69631bcb7d75f697b0dc1
Julius Gonsior <julius@gaenseri.ch>
2020-05-26 18:12:42 +0200

    limit stats

--- train_ann_with_weak_labels.py
+++ train_ann_with_weak_labels.py
@@ -9,41 +9,32 @@ config = standard_config(
     [(["--DATASET_NAME"], {"required": True,}), (["--PICKLE"], {"required": True,}),]
 )
 
+# read in data the same as before (same random inidce)
+X_train, X_test, Y_train, Y_test, label_encoder_classes = get_dataset(
+    config.DATASETS_PATH, config.DATASET_NAME, config.RANDOM_SEED
+)
 
-def weak_label_stats(PICKLE_FILE):
-    Y_train_al = pd.read_pickle(PICKLE_FILE)
 
-    # read in data the same as before (same random inidce)
-    X_train, X_test, Y_train, Y_test, label_encoder_classes = get_dataset(
-        config.DATASETS_PATH, config.DATASET_NAME, config.RANDOM_SEED
-    )
+for filename in os.listdir(config.PICKLE):
+    Y_train_al = pd.read_pickle(config.PICKLE + "/" + filename)
+    # calculate accuracy_score between Y_train_al and Y_train_real
+    amount_of_labels = len(Y_train_al)
+    accuracy = accuracy_score(Y_train.iloc[Y_train_al.index], Y_train_al[0])
+    print("X Len: {:>4} Acc: {:.2f}".format(amount_of_labels, accuracy))
 
-    # calculate accuracy_score betwenn Y_train_al and Y_train_real
-    print(
-        "X Len: {:>4} Acc: {:.2f}".format(
-            len(Y_train_al),
-            accuracy_score(Y_train.iloc[Y_train_al.index], Y_train_al[0]),
-        )
-    )
-
-    # calculate acc per source
-    for source in Y_train_al.source.unique():
-        ys_source = Y_train_al.loc[Y_train_al.source == source]
-        print(
-            source
-            + " Len: {:>4} Acc: {:.2f}".format(
-                len(ys_source),
-                accuracy_score(Y_train.iloc[ys_source.index], ys_source[0],),
+    if amount_of_labels > 500 and accuracy > 0.9:
+        # calculate acc per source
+        for source in Y_train_al.source.unique():
+            ys_source = Y_train_al.loc[Y_train_al.source == source]
+            print(
+                source
+                + " Len: {:>4} Acc: {:.2f}".format(
+                    len(ys_source),
+                    accuracy_score(Y_train.iloc[ys_source.index], ys_source[0],),
+                )
             )
-        )
 
-    # train ann, first with active batches, than with weak batches?
+        # train ann, first with active batches, than with weak batches?
 
-
-if os.path.isdir(config.PICKLE):
-    for filename in os.listdir(config.PICKLE):
-        print(filename)
-        weak_label_stats(config.PICKLE + "/" + filename)
         print("\n")
-else:
-    weak_label_stats(config.PICKLE)
+#  --> select best possible ann candidate based on accuracy and amount of available training data
