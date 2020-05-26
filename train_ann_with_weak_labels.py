d7ea13bb1d4f34006968dd6a8a9adddb7223b75d
Julius Gonsior <julius@gaenseri.ch>
2020-05-26 16:24:18 +0200

    first stats

--- train_ann_with_weak_labels.py
+++ train_ann_with_weak_labels.py
@@ -1,22 +1,49 @@
+import os
 from active_learning.al_cycle_wrapper import train_and_eval_dataset
 from active_learning.experiment_setup_lib import get_dataset, standard_config
 from fake_experiment_oracle import FakeExperimentOracle
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 config = standard_config(
     [(["--DATASET_NAME"], {"required": True,}), (["--PICKLE"], {"required": True,}),]
 )
 
-Y_train_al = pd.read_pickle(config.PICKLE)
 
-# read in data the same as before (same random inidce)
-X_train, X_test, Y_train, Y_test, label_encoder_classes = get_dataset(
-    config.DATASETS_PATH, config.DATASET_NAME, config.RANDOM_SEED
-)
+def weak_label_stats(PICKLE_FILE):
+    Y_train_al = pd.read_pickle(PICKLE_FILE)
 
-print(Y_train_al)
-print(Y_train)
-#  problem: die sample Methode von get_dataset verwendet nicht den random_seed -> muss übergeben werden!!
-# calculate jaccard betwenn Y_train_al and Y_train_real
-# calculate jaccard per source
-# train ann, first with active batches, than with weak batches?
+    # read in data the same as before (same random inidce)
+    X_train, X_test, Y_train, Y_test, label_encoder_classes = get_dataset(
+        config.DATASETS_PATH, config.DATASET_NAME, config.RANDOM_SEED
+    )
+
+    # calculate accuracy_score betwenn Y_train_al and Y_train_real
+    print(
+        "X Len: {:>4} Acc: {:.2f}".format(
+            len(Y_train_al),
+            accuracy_score(Y_train.iloc[Y_train_al.index], Y_train_al[0]),
+        )
+    )
+
+    # calculate acc per source
+    for source in Y_train_al.source.unique():
+        ys_source = Y_train_al.loc[Y_train_al.source == source]
+        print(
+            source
+            + " Len: {:>4} Acc: {:.2f}".format(
+                len(ys_source),
+                accuracy_score(Y_train.iloc[ys_source.index], ys_source[0],),
+            )
+        )
+
+    # train ann, first with active batches, than with weak batches?
+
+
+if os.path.isdir(config.PICKLE):
+    for filename in os.listdir(config.PICKLE):
+        print(filename)
+        weak_label_stats(config.PICKLE + "/" + filename)
+        print("\n")
+else:
+    weak_label_stats(config.PICKLE)
